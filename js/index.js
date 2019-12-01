369144c18b3b58c60e19e4360aaaf0ddf0e7e019
Kate Gavrenko <kategavua@gmail.com>
2019-12-01 22:27:47 +0000

    Add animation, change suits

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 const cardsWrapper = document.querySelector('.cards-wrapper');
 const buttonsWrapper = document.querySelector('.btn-wrapper');
-//const cardsWrapperInner = document.createElement('div');
 let cards = [];
 
 function createCards() {
-  // cardsWrapperInner.style.display = 'none';
   if (document.querySelector('.cards-wrapper-inner') !== null) {
     var element = document.querySelector('.cards-wrapper-inner');
-    
     element.parentNode.removeChild(element);
-  };
+  }
+
   // Create an array with objects containing the value and the suit of each card
   for (let i = 0; i < suits.length; i++) {
     for (let j = 1; j <= 13; j += 1) {
@@ -21,8 +19,8 @@ function createCards() {
       cards.push(cardObject);
     }
   }
-  if (shuffleCards) cards.sort(() => Math.random() - 0.5);
 
+  if (shuffleCards) cards.sort(() => Math.random() - 0.5);
 
   const cardsWrapperInner = document.createElement('div');
   cardsWrapperInner.classList.add('cards-wrapper-inner');
@@ -38,13 +36,13 @@ function createCards() {
     valueTop.classList.add('value-top');
     valueBottom.classList.add('value-bottom');
     cardElement.setAttribute('data-value', card.value);
-    cardElement.classList.add('card', `${card.suit}`);
+    cardElement.classList.add('card', `${card.suit}`, `${card.suit}-${card.value}`);
     cardElement.style.left = `${positionFromLeft}px`;
     cardElement.append(valueTop);
     cardElement.append(valueBottom);
     cardElement.append(cardSuit);
     cardsWrapperInner.append(cardElement);
-    setTimeout(function(){ cardElement.style.left  = "0px" }, 1000);
+    setTimeout(function(){ cardElement.style.left  = "0px" }, 0);
     setTimeout(function(){ cardElement.style.left = `${positionFromLeft}px`; }, 2000);
   });
   cardsWrapper.append(cardsWrapperInner);
@@ -62,7 +60,8 @@ function addTail(symbol) {
 
 // Function to clear out the initial button and create new buttons to play the game.
 function createButtons() {
-  document.querySelector('#start-game').style.display = 'none';
+  let game = document.querySelector('#start-game').style.display = 'none';
+  element.parentNode.removeChild(element);
   const buttons = ['shuffle', 'show-hide', 'magic'];
   for (let btn in buttons) {
     let buttonElement = document.createElement('button');
@@ -74,7 +73,6 @@ function createButtons() {
 
     document.querySelector('#show-hide').addEventListener('click', hideCards);
     document.querySelector('#shuffle').addEventListener('click', function(){
-      // cardsWrapperInner.parentNode.removeChild(cardsWrapperInner);
       shuffle();
       createCards();
       let spades = document.querySelectorAll('.spades');
@@ -84,8 +82,6 @@ function createButtons() {
     });
 
     document.querySelector('#magic').addEventListener('click', function(){
-      // cardsWrapper.style.width = '216px'
-      // cardsWrapperInner.parentNode.removeChild(cardsWrapperInner);
       magic();
       createCards();
       let spades = document.querySelectorAll('.spades');
@@ -100,6 +96,16 @@ function hideCards() {
   showCards = !showCards;
   showCards ? cardsWrapper.classList.add('hidden') :
   cardsWrapper.classList.remove('hidden');
+  let suits = document.querySelectorAll('.card div')
+  if (showCards) {
+    for (let i = 0; i < suits.length; i++) {
+      suits[i].style.display = 'none';
+    }
+  } else {
+    for (let j = 0; j < suits.length; j++) {
+      suits[j].style.display = 'block';
+    }
+  }
 }
 
 let shuffleCards = false;
